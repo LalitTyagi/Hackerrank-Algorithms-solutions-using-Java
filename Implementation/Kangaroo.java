f14f9009ea68345e9f1ee203a041f3935aa44866
Lalit Tyagi <luckytyagi61@gmail.com>
2018-06-12 21:49:44 +0530

    Update Kangaroo.java

--- Implementation/Kangaroo.java
+++ Implementation/Kangaroo.java
@@ -3,12 +3,22 @@ import java.util.*;
 
 public class Kangaroo {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int x1 = in.nextInt();
-        int v1 = in.nextInt();
-        int x2 = in.nextInt();
-        int v2 = in.nextInt();
-        float t=((float)(x2-x1))/((float)(v1-v2));
-        System.out.println((t%1==0&&t>=0)?"YES":"NO");
-    }
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int x1,x2,v1,v2;
+		
+		x1 = in.nextInt();
+		v1 = in.nextInt();
+		x2 = in.nextInt();
+		v2 = in.nextInt();
+		
+		if(v2>v1 && x2>=x1) {
+			System.out.println("NO");
+		}
+		else if(v1>=v2 && ((x2-x1)%((float)(v1-v2))==0)){
+			System.out.println("YES");
+		}
+		else
+			System.out.println("NO");
+	}
 }
